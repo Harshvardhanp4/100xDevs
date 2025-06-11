6251399a9136c537ec08e9ed5739f431d7f4b76c
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-06-11 18:38:46 +0530

    basic js

---
+++ WEEK_1/1.2/main.js
@@ -0,0 +1,20 @@
+//playing with var and booleans to get used to JS
+let firstName ="Harshvardhan";
+let age = 18;
+let isMarried = false;
+
+if(isMarried==true){
+    console.log(firstName +" is married");
+}
+else
+console.log(firstName + " is not married");
+
+//wap that counts from 0-100 and print (for loop)
+
+let ans = 0;
+
+for(let i = 0; i<100;i++){
+    ans = ans+i;
+}
+
+console.log(ans);
