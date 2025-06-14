09ac38c3cc38c310152f4f385d08a7bdabf6fec2
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-06-14 17:58:30 +0530

    basic js apis

---
+++ WEEK_1/1.3/Level 2/Date.js
@@ -0,0 +1,7 @@
+//global date classes of java these are pre made
+
+const currentDate = new Date();
+console.log(currentDate)
+console.log(currentDate.getDate())
+console.log(currentDate.getMonth())
+console.log(currentDate.getFullYear())
