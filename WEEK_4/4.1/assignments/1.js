cf756255c6178a6be5eb7fc73b7c87459dd2524a
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-07-08 15:26:54 +0530

    hard assignments commited

---
+++ WEEK_4/4.1/assignments/1.js
@@ -0,0 +1,25 @@
+const express  = require("express");
+const app = express(); 
+const cors = require("cors");
+const { parse } = require("zod/v4-mini");
+
+
+app.use(cors());
+
+
+app.get("/calculations", (req, res)=>{
+    const principal = parseInt(req.query.principal);
+    const rate = parseInt (req.query.rate)
+    const time = parseInt (req.query.time);
+
+    const interest = (principal*rate*time)/100;
+    const total = (principal + interest);
+
+
+    res.send({
+        total: total, 
+        interest : interest,
+    })
+})
+
+app.listen(3000)
