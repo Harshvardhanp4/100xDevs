47294bd955b3c6a5520934a66a7e54d48704eaef
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-07-21 09:09:40 +0530

    re-renders, memo

--- WEEK_6/6.1_Project/src/App.jsx
+++ WEEK_6/6.1_Project/src/App.jsx
@@ -1,23 +1,73 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+
+
+ 
+
+//   return (
+//     <div>
+//       <HeaderWithButton/>
+//       <Header title = "Harsh2"></Header>
+//     </div> //empty parent and other are children //dpesnt introduce an extra dom element <></>
+//   )
+// }
+
+// function Header({title}){
+//   return <div>
+//     {title}
+//   </div>
+// }//destructured props
+
+// function HeaderWithButton(){
+//     const [title, setTitle] = useState("Harsh");
+     
+//   function updateTitle(){
+//   setTitle("my name is "+ Math.random());
+// }
+//   return <div>
+//     <button onClick={updateTitle}>Click me to change the title</button>
+//       <Header title = {title}></Header>
+//   </div>
+// }
+
+// export default App
+
+
+/// USING MEMO
+import React, {memo} from 'react'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [title, setTitle] = useState("Harsh");
+  
+       
+function updateTitle(){
+  setTitle("my name is "+ Math.random());
+}
 
   return (
     <div>
-      <Header title = "Harsh"></Header>
+      <button onClick={updateTitle}>Click me to change the title</button>
+      <Header title = {title}></Header>
       <Header title = "Harsh2"></Header>
-    </div>
+    </div> //empty parent and other are children //dpesnt introduce an extra dom element <></>
   )
 }
 
-function Header({title}){
+//destructured props
+
+const Header = React.memo(function Header({title}){
   return <div>
     {title}
   </div>
-}
-
+})
+// parent re renders not the individual renders.
 export default App
+
