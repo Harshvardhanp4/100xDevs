5bb12023bd2ba659b22ceab474d75e30a20d870a
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-07-28 10:01:46 +0530

    memo

--- WEEK_6/6.2/codes/src/App.jsx
+++ WEEK_6/6.2/codes/src/App.jsx
@@ -95,17 +95,21 @@
 
 import { useState } from 'react'
 import './App.css'
-
+import { useMemo } from 'react';
 
 function App(){
 
 const [counter , setCounter] = useState(0);
 const [inputValue ,setInputValue] = useState(1);
-  let count = 0;
-  for(let i =0; i<=inputValue; i++){
-    count =  count + i;
-  }
 
+  let count = useMemo(()=>{
+    let count = 0; 
+    for(let i = 0; i<=inputValue; i++){
+      count = count + i;
+
+    }
+    return count;
+  })
   return <div>
     <input onChange = {function(e){
       setInputValue(e.target.value);
