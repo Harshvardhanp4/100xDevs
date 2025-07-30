324f4f4c95d6b1b27d4cd0b6d9a48e06797a44ef
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-07-30 17:00:27 +0530

    useCallback, useMemo

--- WEEK_6/6.2/codes/src/App.jsx
+++ WEEK_6/6.2/codes/src/App.jsx
@@ -33,7 +33,7 @@
 
 
 
-
+//---------------------------------------------------------------------//
 
 // takes todo id as input and fetches the data for that todo from
 // the endpoint and then renders it .
@@ -89,40 +89,148 @@
 
 
 
-//USE MEMO 
+//------------------USE MEMO---------------------// 
 
 
 
-import { useState } from 'react'
-import './App.css'
-import { useMemo } from 'react';
+// import { useState } from 'react'
+// import './App.css'
+// import { useMemo } from 'react';
 
-function App(){
+// function App(){
 
-const [counter , setCounter] = useState(0);
-const [inputValue ,setInputValue] = useState(1);
+// const [counter , setCounter] = useState(0);
+// const [inputValue ,setInputValue] = useState(1);
 
-  let count = useMemo(()=>{
-    let count = 0; 
-    for(let i = 0; i<=inputValue; i++){
-      count = count + i;
+//   let count = useMemo(()=>{
+//     let count = 0; 
+//     for(let i = 0; i<=inputValue; i++){
+//       count = count + i;
 
-    }
-    return count;
-  },[inputValue])
-  return <div>
-    <input onChange = {function(e){
-      setInputValue(e.target.value);
-    }} 
-    placeholder="Sum from 1 to n" />
-    <br />
-    Sum from 1 to {inputValue} is {count}
-    <br />
-    <button onClick={()=>{
-      setCounter(counter+1);
-    }}>Count ({counter})</button>
-  </div>
+//     }
+//     return count;
+//   },[inputValue])
+//   return <div>
+//     <input onChange = {function(e){
+//       setInputValue(e.target.value);
+//     }} 
+//     placeholder="Sum from 1 to n" />
+//     <br />
+//     Sum from 1 to {inputValue} is {count}
+//     <br />
+//     <button onClick={()=>{
+//       setCounter(counter+1);
+//     }}>Count ({counter})</button>
+//   </div>
 
-}
+// }
 
-export default App
+// export default App
+
+
+//---------------UseEffect------------------//
+
+
+// import { useEffect, useState } from 'react'
+// import './App.css'
+// import { useMemo } from 'react';
+
+// function App(){
+
+// const [counter , setCounter] = useState(0);
+// const [inputValue ,setInputValue] = useState(1);
+// const [count , setCount] = useState(0);
+
+//   useEffect(()=>{
+//     let finalCount = 0; 
+//     for(let i = 0; i<=inputValue; i++){
+//       finalCount = finalCount + i;
+//     }
+//     setCount(finalCount);
+//   },[inputValue])
+//   return <div>
+//     <input onChange = {function(e){
+//       setInputValue(parseInt(e.target.value)||0) ;
+//     }} 
+//     placeholder="Sum from 1 to n" />
+//     <br />
+//     Sum from 1 to {inputValue} is {count}
+//     <br />
+//     <button onClick={()=>{
+//       setCounter(counter+1);
+//     }}>Count ({counter})</button>
+//   </div>
+
+// }
+
+
+//--------------------------useCallback--------------------//
+
+// import { useCallback, useEffect, useState } from 'react'
+// import './App.css'
+// import { useMemo } from 'react';
+
+
+// function App(){
+
+// const [counter , setCounter] = useState(0);
+
+
+// const a = useCallback(()=>{
+//   console.log("hi there")
+// },[])
+
+
+//   return <div>
+//     <button onClick={()=>{
+//       setCounter(counter+1);
+//     }}>Count ({counter})</button>
+//     <Demo a={a}/>
+//   </div>
+
+// }
+
+// const Demo = memo(function({a}){
+//   console.log("rerender")
+//   return <div>
+//     hi there
+//   </div>
+// })
+
+
+//-----------------------------useCallback eg 2---------------//
+
+// import { useCallback, useEffect, useState, useMemo, memo } from 'react'
+// import './App.css'
+
+
+
+// const ButtonComponent = memo(({inputFunction})=>{
+//   console.log("child render")
+//   return <div>Button Clicked</div>
+// })
+
+// function App(){
+
+// const [count , setCount] = useState(0);
+
+
+// const inputFunction = useCallback(()=>{
+//   console.log("hi there")
+// },[]) // this doesnt allow this native function to re render.
+
+
+//     return  <div>
+//       <ButtonComponent inputFunction ={inputFunction}/>
+//     <button onClick={()=>{
+//       setCount(count +1);
+//     }}>Click me {count}</button></div>
+// }
+
+
+// export default App;
+
+
+
+
+//-------------------------------Custom Hooks---------------------------------//
