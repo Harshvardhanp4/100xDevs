d771843d7173a7be5dd11d1b0a2125b8dc1fa07c
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-07-30 23:34:13 +0530

    useMemo

--- WEEK_6/6.3/codes/src/App.jsx
+++ WEEK_6/6.3/codes/src/App.jsx
@@ -24,59 +24,117 @@
 
 
 
+
+
+
+
 //---------------------useEffect---------------------//
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useEffect } from 'react';
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import { useEffect } from 'react';
 
-function App() {
+// function App() {
   
-  const [exchangeData,  setExchangeData] = useState({});
-  const [bankData,  setBankData] = useState({});
+//   const [exchangeData,  setExchangeData] = useState({});
+//   const [bankData,  setBankData] =  useState({});
 
-  console.log("re - render")
- // this code base will cause multiple re renders   
-  // setTimeout(()=>{
-  //   setBankData({
-  //     income: 100
-  //   });
-  // }, 3000);
+//   console.log("re - render")
+//  // this code base will cause multiple re renders   
+//   // setTimeout(()=>{
+//   //   setBankData({
+//   //     income: 100
+//   //   });
+//   // }, 3000);
 
-  // setTimeout(()=>{
-  //   setExchangeData({
-  //     returns: 100
-  //   });
-  // }, 1000); //this code base will cause multiple re renders
+//   // setTimeout(()=>{
+//   //   setExchangeData({
+//   //     returns: 100
+//   //   });
+//   // }, 1000); //this code base will cause multiple re renders
 
 
-  // we use useEffect here
+//   // we use useEffect here this will not cause multiple re renders
 
-  useEffect(()=>{ 
-  setTimeout(()=>{
-    setBankData({
-      income: 100
-    });
-  }, 3000);
-  },[])
+//   useEffect(()=>{ 
+//   setTimeout(()=>{
+//     setBankData({
+//       income: 100
+//     });
+//   }, 3000);
+//   },[])
+
+//   useEffect(()=>{
+//     setTimeout(()=>{
+//     setExchangeData({
+//       returns: 100
+//     });
+//   }, 1000);
+//   },[])
+  
+//   const incomeTax = (bankData.income  + exchangeData.returns) * 0.3;
+//   return(
+//   <div>
+//     hi there your income tax returns are {incomeTax}
+//   </div>
+//   )
+  
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+//-----------------------------------useMemo--------------------------------------/
+
+import { useEffect, useMemo, useState } from "react";
+
+function App (){
+  const[exchangeData, setExchangeData] = useState({});
+  const[exchange2Data, setExchange2Data] = useState({});
+  const[bankData, setBankData] = useState({});
+
 
   useEffect(()=>{
-    setTimeout(()=>{
     setExchangeData({
       returns: 100
     });
-  }, 1000);
-  },[])
+  }, [])
+
+  useEffect(()=>{
+    setExchange2Data({
+      returns: 100
+    });
+  }, [])
+
+  useEffect(()=>{
+    setTimeout(()=>{
+       setBankData({
+      income: 100
+    });
+    }, 5000)
+   
+  }, [])
   
-  const incomeTax = (bankData.income  + exchangeData.returns) * 0.3;
-  return(
-  <div>
-    hi there your income tax returns are {incomeTax}
+  const cryptoReturns  = useMemo(()=>{
+    console.log(" hey rerender")
+    return exchangeData.returns + exchange2Data.returns;
+  },[exchangeData,exchange2Data]) ;
+
+  const incomeTax = (cryptoReturns + bankData.income)*0.3;
+  
+
+  return <div>
+  Your income tax returns are {incomeTax}
   </div>
-  )
-  
+
 }
 
-export default App
+export default App;
