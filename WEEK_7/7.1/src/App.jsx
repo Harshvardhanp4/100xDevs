fa3775cc36ed4632fd7ce3794c8157dcc16d5595
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-08-03 16:15:53 +0530

    SuspenseApi ,PropDrilling, ContextApi

--- WEEK_7/7.1/src/App.jsx
+++ WEEK_7/7.1/src/App.jsx
@@ -30,6 +30,7 @@
 // export default App
 
 
+//----------------------------------------------------------------------------------------------------//
 
 //----So to solve this we use useNavigate hook. Lets you navigate from one route to another
 //its taken from react-router-dom library. Chages the oage instead of a hard reload this 
@@ -75,40 +76,153 @@
 // export default App
 
 
-//Suspense API
 
 
-import { lazy, Suspense } from 'react'
-import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
+//------------------------------Suspense API-----------------------------------------------------//
+
+
+// import { lazy, Suspense } from 'react'
+// import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
+// import './App.css'
+
+// const Dashboard = lazy(()=> import('./components/Dashboard'))
+// const Landing = lazy(()=> import('./components/Landing'))
+
+
+// function App() {
+ 
+//   return (
+//     <div>
+//     <BrowserRouter>
+//     <Routing></Routing>
+//     <Routes>
+//       <Route path="/dashboard" element ={<Suspense fallback ={"loading.."}><Dashboard/></Suspense>}/>
+//       <Route path="/" element ={<Landing/>}/>
+//     </Routes>
+//     </BrowserRouter>
+  
+//     </div>
+//   )
+// }
+
+// function Routing(){
+//     const navigate = useNavigate();// can only be used inside browserrouter
+//   return <div>
+//       <div>
+//         <button onClick={()=>{navigate("/");}}>Landing Page</button>
+//         <button onClick={()=>{navigate("/dashboard");}}>Dashboard</button>
+//       </div>
+//   </div>
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+//--------------------------Prop Drilling----------------------------------------------------------//
+
+
+
+
+
+// import { useState } from 'react'
+// import './App.css'
+
+
+
+// function App() {
+ 
+//     const[count, setCount] =  useState(0);
+
+//   return (
+//     <div>
+//     <Count count = {count}  setCount={setCount}/>
+    
+//     </div>
+//   )
+// }
+
+// function Count({count, setCount}){ // here you can see that count doesnt need the setCount but buttons need it so we have drilled the prop down not a good approach but for learning App(parent)-> Child(Count)-> Button->(Child of Count)
+//   return <div>
+//     {count}
+//     <Buttons count = {count} setCount={setCount}/>
+//   </div>
+// }
+
+// function Buttons({count, setCount}){
+//   return <div>
+//     <button onClick={()=>{
+//       setCount(count + 1);
+//     }}>Increase</button>
+
+//     <button onClick={()=>{
+//       setCount(count - 1);
+//     }}>Decrease</button>
+
+//   </div>
+// }
+
+// export default App
+
+
+
+
+
+
+//---------------------------------Context API-------------------------------------------------------//
+// go to components for context.jsx
+
+import { useState } from 'react'
 import './App.css'
+import { CountContext } from './components/context';
+import { useContext } from 'react';
 
-const Dashboard = lazy(()=> import('./components/Dashboard'))
-const Landing = lazy(()=> import('./components/Landing'))
 
 
 function App() {
  
+    const[count, setCount] =  useState(0);
+
+
   return (
     <div>
-    <BrowserRouter>
-    <Routing></Routing>
-    <Routes>
-      <Route path="/dashboard" element ={<Suspense fallback ={"loading.."}><Dashboard/></Suspense>}/>
-      <Route path="/" element ={<Landing/>}/>
-    </Routes>
-    </BrowserRouter>
-  
+    <CountContext.Provider value ={count}>
+    <Count setCount={setCount}/>
+    </CountContext.Provider>
     </div>
   )
 }
 
-function Routing(){
-    const navigate = useNavigate();// can only be used inside browserrouter
+function Count({ setCount}){ 
   return <div>
-      <div>
-        <button onClick={()=>{navigate("/");}}>Landing Page</button>
-        <button onClick={()=>{navigate("/dashboard");}}>Dashboard</button>
-      </div>
+    <CountRenderer/>
+    <Buttons setCount={setCount}/>
+  </div>
+}
+
+function CountRenderer(){
+  const count = useContext(CountContext);
+  return <div>
+    {count}
+  </div>
+}
+
+function Buttons({setCount}){
+  const count = useContext(CountContext);  
+  return <div>
+    <button onClick={()=>{
+      setCount(count + 1);
+    }}>Increase</button>
+
+    <button onClick={()=>{
+      setCount(count - 1);
+    }}>Decrease</button>
+
   </div>
 }
 
