c63991524bb066d1305d2754ed4a3f2cecc6e3bd
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-08-03 09:52:28 +0530

    useNavigate, Routing

---
+++ WEEK_7/7.1/src/App.jsx
@@ -0,0 +1,75 @@
+
+// This is not an optimal approach window.location this will refetch the index.html
+
+// import {BrowserRouter, Routes, Route} from 'react-router-dom'
+// import './App.css'
+// import { Dashboard } from './components/Dashboard'
+// import { Landing } from './components/Landing'
+
+// function App() {
+ 
+
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={()=>{window.location.href="/"}}>Landing Page</button>
+//         <button onClick={()=>{window.location.href="/dashboard"}}>Dashboard</button>
+//       </div>
+//     <BrowserRouter>
+//     <Routes>
+//       <Route path="/dashboard" element ={<Dashboard/>}/>
+//       <Route path="/" element ={<Landing/>}/>
+//     </Routes>
+//     </BrowserRouter>
+//     </div>
+//   )
+// }
+
+
+
+// export default App
+
+
+
+//----So to solve this we use useNavigate hook. Lets you navigate from one route to another
+//its taken from react-router-dom library. Chages the oage instead of a hard reload this 
+//is the optimal way to do client side routing
+
+
+
+
+
+import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
+import './App.css'
+import { Dashboard } from './components/Dashboard'
+import { Landing } from './components/Landing'
+
+function App() {
+ 
+
+
+  return (
+    <div>
+    <BrowserRouter>
+    <Routing></Routing>
+    <Routes>
+      <Route path="/dashboard" element ={<Dashboard/>}/>
+      <Route path="/" element ={<Landing/>}/>
+    </Routes>
+    </BrowserRouter>
+  
+    </div>
+  )
+}
+
+function Routing(){
+    const navigate = useNavigate();// can only be used inside browserrouter
+  return <div>
+      <div>
+        <button onClick={()=>{navigate("/");}}>Landing Page</button>
+        <button onClick={()=>{navigate("/dashboard");}}>Dashboard</button>
+      </div>
+  </div>
+}
+
+export default App
