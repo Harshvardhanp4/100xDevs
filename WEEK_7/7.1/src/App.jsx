f3622313071b4942c12f1396a462b9207d5b28a9
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-08-03 15:16:10 +0530

    routing, suspense api

--- WEEK_7/7.1/src/App.jsx
+++ WEEK_7/7.1/src/App.jsx
@@ -38,21 +38,62 @@
 
 
 
-import { lazy } from 'react'
+// import { lazy } from 'react'
+// import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
+// import './App.css'
+
+// const Dashboard = lazy(()=> import('./components/Dashboard'))
+// const Landing = lazy(()=> import('./components/Landing'))
+
+
+// function App() {
+ 
+//   return (
+//     <div>
+//     <BrowserRouter>
+//     <Routing></Routing>
+//     <Routes>
+//       <Route path="/dashboard" element ={<Dashboard/>}/>
+//       <Route path="/" element ={<Landing/>}/>
+//     </Routes>
+//     </BrowserRouter>
+  
+//     </div>
+//   )
+// }
+
+// function Routing(){
+//     const navigate = useNavigate();// can only be used inside browserrouter
+//   return <div>
+//       <div>
+//         <button onClick={()=>{navigate("/");}}>Landing Page</button>
+//         <button onClick={()=>{navigate("/dashboard");}}>Dashboard</button>
+//       </div>
+//   </div>
+// }
+
+// export default App
+
+
+//Suspense API
+
+
+import { lazy, Suspense } from 'react'
 import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
 import './App.css'
+
 const Dashboard = lazy(()=> import('./components/Dashboard'))
 const Landing = lazy(()=> import('./components/Landing'))
+
+
 function App() {
  
-
-
   return (
     <div>
     <BrowserRouter>
     <Routing></Routing>
     <Routes>
-      <Route path="/dashboard" element ={<Dashboard/>}/>
+      <Route path="/dashboard" element ={<Suspense fallback ={"loading.."}><Dashboard/></Suspense>}/>
       <Route path="/" element ={<Landing/>}/>
     </Routes>
     </BrowserRouter>
