c7bc9ea983db86be1a2e0fd485f3cb288fc42651
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-08-07 18:46:22 +0530

    atom family

---
+++ WEEK_7/7.4-advance/src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+import { RecoilRoot, useRecoilValue } from 'recoil'
+import { todosAtomFamily } from './store/atom'
+
+function App() {
+
+  return  <RecoilRoot>
+    <Todo id = {1}/>
+    <Todo id = {2}/>
+    <Todo id = {2}/>
+    <Todo id = {2}/>
+  </RecoilRoot>
+ 
+  
+}
+
+function Todo({id}) {
+  const currentTodo = useRecoilValue(todosAtomFamily(id));
+
+    return (
+      <>
+      {currentTodo.title}
+      {currentTodo.description}
+      </>
+    )
+}
+
+export default App
---
+++ WEEK_7/7.4-advance/src/todos.js
@@ -0,0 +1,9 @@
+export const TODOS =[{
+    id: 1,
+    title: "Go to Gym",
+    description: "From 6- 8"
+},{
+    id: 2,
+    title: "go to eat food",
+    description: "from 9-10"
+}]
