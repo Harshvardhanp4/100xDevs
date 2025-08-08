d71d9c026a2035077b82d7ff44d0a32146c8cd69
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-08-08 22:53:33 +0530

    youtube-clone

---
+++ WEEK_8/8.0/src/videos.ts
@@ -0,0 +1,66 @@
+export interface Video {
+    title: string;
+    thumbnail: string;
+    description: string;
+    viewCount: string;
+    timestamp: string;
+}
+
+export const VIDEOS: Video[] = [
+    {
+        title: "Beach Songs",
+        thumbnail: "/video1.jpg",
+        description: "Mix Song of Beach Songs",
+        viewCount: "100k",
+        timestamp: "4 hours ago"
+    },
+     {
+        title: "Beach Songs",
+        thumbnail: "/video1.jpg",
+        description: "Mix Song of Beach Songs",
+        viewCount: "100k",
+        timestamp: "4 hours ago"
+    }, {
+        title: "Beach Songs",
+        thumbnail: "/video1.jpg",
+        description: "Mix Song of Beach Songs",
+        viewCount: "100k",
+        timestamp: "4 hours ago"
+    }, {
+        title: "Beach Songs",
+        thumbnail: "/video1.jpg",
+        description: "Mix Song of Beach Songs",
+        viewCount: "100k",
+        timestamp: "4 hours ago"
+    }, {
+        title: "Beach Songs",
+        thumbnail: "/video1.jpg",
+        description: "Mix Song of Beach Songs",
+        viewCount: "100k",
+        timestamp: "4 hours ago"
+    }, {
+        title: "Beach Songs",
+        thumbnail: "/video1.jpg",
+        description: "Mix Song of Beach Songs",
+        viewCount: "100k",
+        timestamp: "4 hours ago"
+    }, {
+        title: "Beach Songs",
+        thumbnail: "/video1.jpg",
+        description: "Mix Song of Beach Songs",
+        viewCount: "100k",
+        timestamp: "4 hours ago"
+    }, {
+        title: "Beach Songs",
+        thumbnail: "/video1.jpg",
+        description: "Mix Song of Beach Songs",
+        viewCount: "100k",
+        timestamp: "4 hours ago"
+    }, {
+        title: "Beach Songs",
+        thumbnail: "/video1.jpg",
+        description: "Mix Song of Beach Songs",
+        viewCount: "100k",
+        timestamp: "4 hours ago"
+    },
+]
