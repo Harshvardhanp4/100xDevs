c30dc4bf237dd00e4643743f3110b63065e8113c
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-08-20 00:23:57 +0530

    signup-endpoint created

--- WEEK_8/8.2/frontend/src/pages/Signup.jsx
+++ WEEK_8/8.2/frontend/src/pages/Signup.jsx
@@ -3,20 +3,44 @@ import { Heading } from "../components/Heading"
 import { InputBox } from "../components/InputBox"
 import { SubHeading } from "../components/SubHeading"
 import { Button } from "../components/Button"
+import axios from "axios"
+import { useState } from "react"
 
 export const Signup = () =>{
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
     return <div className="bg-slate-300 h-screen flex justify-center">
         <div className="flex flex-col justify-center">
             <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
                 <Heading label={"Sign Up"}></Heading>
                 <SubHeading label={"Enter your information to create an account"}></SubHeading>
-                <InputBox placeholder="Harsh" label={"First Name"}></InputBox>
-                <InputBox placeholder="Patil" label={"Last Name"}></InputBox>   
-                <InputBox placeholder="harsh@gmail.com" label={"Email"}></InputBox>   
-                <InputBox placeholder="password@123" label={"Password"}></InputBox>   
-            <div className="pt-4">
-                <Button label= {"Sign Up"}></Button>
-            </div>
+                <InputBox onChange={e=>{
+                  setFirstName(e.target.value);  
+                }}placeholder="Harsh" label={"First Name"}></InputBox>
+                <InputBox onChange={e=>{
+                setLastName(e.target.value);
+                }}placeholder="Patil" label={"Last Name"}></InputBox>   
+                <InputBox onChange={e=>{
+                setUsername(e.target.value);
+                }} placeholder="harsh@gmail.com" label={"Email"}></InputBox>   
+                <InputBox onChange={e=>{
+                setPassword(e.target.value);
+                }}placeholder="password@123" label={"Password"}></InputBox>   
+                 <div className="pt-4">
+                    <Button onClick={()=>{
+                        axios.post("http://localhost:3000/api/v1/user/signup",{
+                              
+                            firstName,
+                            lastName,
+                            username,
+                            password
+
+                        })
+                    }} label= {"Sign Up"}></Button>
+                </div>
         
             <BottomWarning label={"Already have an account?"} buttonText={"Sign In"} to={"/signin"}></BottomWarning>
             </div>
