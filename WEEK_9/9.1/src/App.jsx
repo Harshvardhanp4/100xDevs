72ef700021bd418683f773378f9f1bf146473236
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-08-23 19:43:29 +0530

    custom hooks

--- WEEK_9/9.1/src/App.jsx
+++ WEEK_9/9.1/src/App.jsx
@@ -4,6 +4,8 @@
 // import './App.css'
 // import React from 'react'
 
+// import { useEffect, useState } from "react";
+
 // function App() {
 
 //   // const [render, setRender] = useState(true);
@@ -110,52 +112,260 @@
 
 
 //-----------------------------------Custom Hooks------------------------------------------------------//
-import { useEffect, useState } from "react"
-import axios from "axios";
+// import { useEffect, useState } from "react"
+// import axios from "axios";
 
 
-function App() {
+// function App() {
 
-  const {todos, loading} = useTodos([]);
-  if(loading){  
-    return <div>loading...</div>
-  }
+//   const {todos, loading} = useTodos([]);
+//   if(loading){  
+//     return <div>loading...</div>
+//   }
 
-  return (
-    <>
-      {todos.map(todo => <Track key={todo.id} todo={todo} />)}
-    </>
-  )
-}
+//   return (
+//     <>
+//       {todos.map(todo => <Track key={todo.id} todo={todo} />)}
+//     </>
+//   )
+// }
 
 
-const Track = ({ todo }) => {
-  return <div>
-    {todo.title}
-    <br />
-    {todo.description}
-  </div>
-}
+// const Track = ({ todo }) => {
+//   return <div>
+//     {todo.title}
+//     <br />
+//     {todo.description}
+//   </div>
+// }
 
-//data fetching hook a custom hook
+// //data fetching hook a custom hook
 
-function useTodos() {
-  const [todos, setTodos] = useState([])
-  const [loading, setLoading] = useState(true);
+// function useTodos() {
+//   const [todos, setTodos] = useState([])
+//   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setTimeout(()=>{
-       axios.get("http://localhost:5000/todos")
-      .then(res => {
-        setTodos(res.data);
-        setLoading(false); 
-      })
-  }, 5000)
-    },[])
+//   useEffect(() => {
+//     setTimeout(()=>{
+//        axios.get("http://localhost:5000/todos")
+//       .then(res => {
+//         setTodos(res.data);
+//         setLoading(false); 
+//       })
+//   }, 5000)
+//     },[])
    
 
-  return {todos, loading};
+//   return {todos, loading};
 
-}
+// }
 
-export default App
+// export default App 
+
+
+
+//------------------------------Auto Refresh Custom Hook ---------------------------------------//
+
+
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+
+// function App() {
+//   const { todos, loading } = useTodos(2); // pass interval in seconds (e.g. 2)
+
+//   if (loading) {
+//     return <div>loading...</div>;
+//   }
+
+//   return (
+//     <>
+//       {todos.map((todo) => (
+//         <Track key={todo.id} todo={todo} />
+//       ))}
+//     </>
+//   );
+// }
+
+// const Track = ({ todo }) => {
+//   return (
+//     <div>
+//       {todo.title}
+//       <br />
+//       {todo.description}
+//     </div>
+//   );
+// };
+
+// // custom data fetching hook
+// function useTodos(n) {
+//   const [todos, setTodos] = useState([]);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const fetchTodos = () => {
+//       axios.get("http://localhost:5000/todos")
+//         .then((res) => {
+//           setTodos(res.data);
+//           setLoading(false);
+//         })
+//         .catch(() => {
+//           setLoading(false);
+//         });
+//     };
+
+//     fetchTodos(); // fetch immediately once
+//     const intervalId = setInterval(fetchTodos, n * 5000);
+
+//     return () => clearInterval(intervalId); // cleanup
+//   }, [n]);
+
+//   return { todos, loading };
+// }
+
+// export default App;
+
+
+
+//----------------------------useIsOnline Hook-------------------------------------//
+// hook which returns true or false based on whether user is online or offline
+// import { useState, useEffect } from "react";
+
+// function App(){
+//   const isOnline = useIsOnline();
+//   if (isOnline){
+//     return "you are online"
+//   }
+
+//   return "you are offline"
+  
+// }
+
+// function useIsOnline(){
+//   const [isOnline, setIsOnline] = useState(window.navigator.onLine);
+
+//   useEffect(()=>{
+//     window.addEventListener("online",()=>{
+//       setIsOnline(true);
+//     })
+//     window.addEventListener("offline",()=>{
+//       setIsOnline(false);
+//     })
+//   },[])
+
+//   return isOnline
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+
+//----------------------------------useMousePointer----------------------------------------//
+//tells the axis at where mouse pointer is 
+
+
+
+// import { useState, useEffect } from "react";
+
+// const useMousePointer=()=>{
+//   const [position, setPosition] = useState({x:0,y:0});
+
+//   const handleMouseMove = (e)=>{
+//     setPosition({x: e.clientX, y: e.clientY});
+//   }
+
+//   useEffect(()=>{
+//     window.addEventListener('mousemove', handleMouseMove);
+//     return ()=>{
+//       window.removeEventListener('mousemove', handleMouseMove);
+//     }
+//   },[]);
+//   return position
+// }
+
+// function App(){
+  
+//   const mousePointer = useMousePointer();
+//   return(
+//     <>
+//     Your mouse pointer is {mousePointer.x} {mousePointer.y}
+//     </>
+//   )
+// }
+
+
+// export default App
+
+
+//-------------------------------------useInterval Hook----------------------------------------//
+// A hook that return a certain callback function in every n seconds
+
+// import { useEffect, useState } from "react";
+
+// function App(){
+//   const [count , setCount] = useState(0);
+  
+//   useInterval(()=>{
+//     setCount(c=>c+1);
+//   },1000)
+
+//   return (
+//     <>
+//     Timer is at {count}
+//     </>
+//   )
+// }
+
+// const useInterval = (callback) =>{
+//   useEffect(()=>{
+//   const interval = setInterval(callback, 1000);
+//   return ()=> clearInterval(interval);  
+//   },[callback])
+// }
+
+// export default App
+
+
+
+
+
+//---------------------------------useDebounce--------------------------------------------------//
+//used in amazon searchbar thingy ..
+
+// import { useEffect, useState } from "react";
+
+// function useDebounce(value, timeout){
+//   const [debouncedValue, setDebouncedValue] =useState(value);
+  
+//   useEffect(()=>{
+//     let timeoutNumber = setTimeout(()=>{
+//       setDebouncedValue(value);
+//     }, timeout)
+//      return ()=>{
+//     clearTimeout(timeoutNumber);
+//   }
+//   },[value])
+
+ 
+//   return debouncedValue;
+// }
+
+// function App(){
+//   const [value, setValue] = useState();
+//   const debouncedValue = useDebounce(value, 500);
+
+//   return (
+//     <>
+//     Debounce value is {debouncedValue}<br></br>
+//     <input type="text" onChange={e=>setValue(e.target.value) }/>
+//     </>
+//   )
+// }
+
+
+// export default App
