498b07f9543908320ebbf5a4f5d8c9fa97313725
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-08-28 12:27:42 +0530

    POSTGRES

---
+++ WEEK_10/10.1/dist/joins/utils.js
@@ -0,0 +1,8 @@
+import { Client } from 'pg';
+export async function getClient() {
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL
+    });
+    await client.connect();
+    return client;
+}
---
+++ WEEK_10/10.1/src/joins/create-table.ts
@@ -0,0 +1,17 @@
+import { getClient } from "./utils.js";
+import "dotenv/config";
+
+async function createTable(){
+    const client = await getClient();
+    const createUserTableQuery = `
+    CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL, 
+    password VARCHAR(255) NOT NULL
+    );
+    `;
+ await client.query(createUserTableQuery);
+ console.log("Table created successfully")
+}
+
+createTable();
