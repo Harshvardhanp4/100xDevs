2b599d2b7edc035043bc3a6fe35e40a678a78093
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-09-03 10:34:20 +0530

    FOREIGN-KEY-RECAP

--- WEEK_10/10.1.2/dist/index.js
+++ WEEK_10/10.1.2/dist/index.js
@@ -1,6 +1,5 @@
 import { Client } from "pg";
 import dotenv from "dotenv";
-import { email } from "zod";
 dotenv.config();
 const client = new Client({
     connectionString: process.env.DATABASE_URL
@@ -31,26 +30,49 @@ const client = new Client({
 // insertIntoUserTable('u1@gmail.com', 'pass2');
 //---------------------GET USERS-------------------------//
 //optimal way 
-async function getUserFromEmail(email) {
-    try {
-        await client.connect();
-        const query = 'SELECT * FROM users WHERE email = $1';
-        const result = await client.query(query, [email]);
-        if (result.rows.length > 0) {
-            console.log('User found: ', result.rows[0]);
-            return result.rows[0];
-        }
-        else {
-            console.log('User not found');
-            return null;
-        }
-    }
-    catch (err) {
-        console.error('Error', err);
-        throw err;
-    }
-    finally {
-        await client.end();
-    }
+// async function getUserFromEmail(email: string){
+//     try{
+//     await client.connect();
+//     const query = 'SELECT * FROM users WHERE email = $1';
+//     const result = await client.query(query, [email]);    
+//      if(result.rows.length>0){
+//         console.log('User found: ', result.rows[0]);
+//         return result.rows[0];
+//     }
+//     else{
+//         console.log('User not found')
+//         return null;
+//     }
+//     }catch(err){
+//         console.error('Error',err);
+//         throw err;
+//     }
+//     finally{
+//         await client.end();
+//     }
+// }
+// getUserFromEmail('u1@gmail.com')
+//------------------RELATIONSHIPS & TRANSACTIONS-----------------------//
+// CREATING A ADDRESS TABLE WITH FOREIGN KEY 
+// async function createAddressTable(){
+//     await client.connect();
+//     const query = `CREATE TABLE address(
+//     id SERIAL PRIMARY KEY,
+//     user_id INTEGER NOT NULL,
+//     city VARCHAR(255) NOT NULL,
+//     FOREIGN KEY (user_id) REFERENCES users(id)
+//     )`;
+//     await client.query(query);
+//     console.log("Address table created!");
+//     await client.end();
+// }
+// createAddressTable();
+async function insertAddress(user_id, city) {
+    await client.connect();
+    const query = `INSERT INTO address(user_id, city) VALUES($1, $2)`;
+    const value = [user_id, city];
+    const res = await client.query(query, value);
+    console.log("Insertion success!", res);
+    await client.end();
 }
-getUserFromEmail('u1@gmail.com');
+insertAddress(2, 'pune');
--- WEEK_10/10.1.2/src/index.ts
+++ WEEK_10/10.1.2/src/index.ts
@@ -46,29 +46,63 @@ const client = new Client({
 
 //---------------------GET USERS-------------------------//
 //optimal way 
-async function getUserFromEmail(email: string){
-    try{
-    await client.connect();
-    const query = 'SELECT * FROM users WHERE email = $1';
-    const result = await client.query(query, [email]);    
+// async function getUserFromEmail(email: string){
+//     try{
+//     await client.connect();
+//     const query = 'SELECT * FROM users WHERE email = $1';
+//     const result = await client.query(query, [email]);    
         
-     if(result.rows.length>0){
-        console.log('User found: ', result.rows[0]);
-        return result.rows[0];
-    }
-    else{
-        console.log('User not found')
-        return null;
-    }
-    }catch(err){
-        console.error('Error',err);
-        throw err;
-    }
-    finally{
-        await client.end();
-    }
+//      if(result.rows.length>0){
+//         console.log('User found: ', result.rows[0]);
+//         return result.rows[0];
+//     }
+//     else{
+//         console.log('User not found')
+//         return null;
+//     }
+//     }catch(err){
+//         console.error('Error',err);
+//         throw err;
+//     }
+//     finally{
+//         await client.end();
+//     }
    
     
-}
+// }
 
-getUserFromEmail('u1@gmail.com')
+// getUserFromEmail('u1@gmail.com')
+
+
+//------------------RELATIONSHIPS & TRANSACTIONS-----------------------//
+
+// CREATING A ADDRESS TABLE WITH FOREIGN KEY 
+
+// async function createAddressTable(){
+//     await client.connect();
+//     const query = `CREATE TABLE address(
+//     id SERIAL PRIMARY KEY,
+//     user_id INTEGER NOT NULL,
+//     city VARCHAR(255) NOT NULL,
+//     FOREIGN KEY (user_id) REFERENCES users(id)
+//     )`;
+//     await client.query(query);
+//     console.log("Address table created!");
+//     await client.end();
+// }
+// createAddressTable();
+
+
+// Insert into Address.
+
+ async function insertAddress(user_id:number, city: string){
+     await client.connect();
+
+     const query = `INSERT INTO address(user_id, city) VALUES($1, $2)`;
+     const value = [user_id, city];
+     const res = await client.query(query,value);
+     console.log("Insertion success!",res);
+     await client.end();
+
+ }
+ insertAddress(2,'pune')
