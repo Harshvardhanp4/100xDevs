4dd3b4f8d31f4a499cada32d8f01541407312bb1
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-09-07 23:21:30 +0530

    Hono-App

--- WEEK_11/11.1/hono-app/src/index.ts
+++ WEEK_11/11.1/hono-app/src/index.ts
@@ -2,8 +2,23 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+async function authMiddleware (c:any,next:any){
+  if(c.req.header("Authorization")){
+    await next()
+  }else{
+    return c.text("dont have access")
+  }
+}
+
+app.use(authMiddleware);
+
+
+app.post('/', async(c) => {
+  const body = await c.req.json()
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
   return c.text('Hello Hono!')
 })
-
+// c gives both req,res params
 export default app
