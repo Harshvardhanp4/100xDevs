15eb10b5ed69409dbb253db89daf6ec7a13d7f18
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-09-18 15:03:16 +0530

    Prisma Recap, Relationships

--- WEEK_12/12.2/12.2.2/dist/index.js
+++ WEEK_12/12.2/12.2.2/dist/index.js
@@ -1,16 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-
-async function insertUser(username: string, password: string, firstName: string, lastName: string, email: string){
+async function insertUser(username, password, firstName, lastName, email) {
     const response = await prisma.user.create({
-        data:{
+        data: {
             username,
             password,
             firstName,
             lastName,
             email
         }
-    })
+    });
     console.log(response);
 }
-insertUser('harshvardhan','pass123', 'harsh','p','harsh123@gmail.com')
+insertUser('harshvardhan', 'pass123', 'harsh', 'p', 'harsh123@gmail.com');
+//# sourceMappingURL=index.js.map
---
+++ WEEK_12/12.2/12.2.2/src/todos.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+// async function createTodo(user_id: number, title: string, description: string, done:boolean){
+
+//     const response = await prisma.todo.create({
+//         data:{
+//             user_id,
+//             title,
+//             description, 
+//             done
+//         }
+//     })
+//     console.log(response);
+
+// }
+// createTodo(1,'go to sleep', 'from 11',true)
+
+
+
+async function getTodos(user_id: number){
+    const response = await prisma.todo.findMany({
+        where: {
+            user_id 
+        },
+        select:{
+            id: true,
+            title: true,
+            description: true,
+            user: true
+        }
+    })
+   console.log(response);
+}
+getTodos(1);
