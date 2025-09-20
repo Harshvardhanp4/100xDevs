1a1cbca0ab6a97433144c357b7dccaecdf097629
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-09-21 01:59:25 +0530

    Endpoints&AuthDone

--- WEEK_13/backend/dist/index.js
+++ WEEK_13/backend/dist/index.js
@@ -1,29 +1,49 @@
+import prisma from "./prismaClient.js";
 import express from "express";
+import jwt from "jsonwebtoken";
+import { authMiddleware } from "./middlewares/auth.js";
 const PORT = 3000;
 const app = express();
 app.use(express.json());
 // User Endpoints
-app.post("/api/v1/signup", (req, res) => {
-    console.log("Signup Endpoint");
-    res.send("Signup Endpoint");
+app.post("/api/v1/signup", async (req, res) => {
+    const { name, email, password } = req.body;
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password
+        }
+    });
+    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET);
+    res.json({ user, token: `Bearer ${token}` });
 });
-app.post("api/v1/signin", (req, res) => {
-    console.log("Signin Endpoint");
-    res.send("Signin Endpoint");
+app.post("/api/v1/signin", async (req, res) => {
+    const { email, password } = req.body;
+    const user = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    });
+    if (!user || user.password !== password) {
+        return res.status(401).json({ message: "Invalid Credentials" });
+    }
+    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET);
+    res.json({ user, token: `Bearer ${token}` });
 });
 //Blog Endpoints
 //create blog
-app.post("/api/v1/blogs", (req, res) => {
+app.post("/api/v1/blogs", authMiddleware, async (req, res) => {
     console.log("Create Blog Endpoint");
     res.send("Create Blog Endpoint");
 });
 //Update blog
-app.put("/api/v1/blogs/", (req, res) => {
+app.put("/api/v1/blogs/", authMiddleware, (req, res) => {
     console.log("Update Blog Endpoint");
     res.send("Update Blog Endpoint");
 });
 //Get blog by id
-app.get("/api/v1/blogs/:id", (req, res) => {
+app.get("/api/v1/blogs/:id", authMiddleware, (req, res) => {
     console.log("Get All Blogs Endpoint");
     res.send("Get All Blogs Endpoint");
 });
---
+++ WEEK_13/backend/dist/routes/blogRoutes.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=blogRoutes.d.ts.map
