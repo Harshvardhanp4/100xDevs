a464eadc905848d9f97ecadd75b0f309298308a4
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-09-23 10:02:07 +0530

    signup/signin redirects, tokens,done

--- WEEK_13/frontend/src/components/Auth.tsx
+++ WEEK_13/frontend/src/components/Auth.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, Navigate, useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { type SignupInput } from "@harshvardhanp4/medium-common";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
